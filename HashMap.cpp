116d0d7cf2357c1fac110a4cc4d82cc4e7036e83
jroker-x <hakkemwasif@gmail.com>
2026-06-19 10:39:12 +0530

    Project #2 HashMap

--- HashMap.cpp
+++ HashMap.cpp
@@ -2,10 +2,12 @@
 #include<vector>
 
 size_t hash(const std::string& str);
+
 template<typename k,typename v>
 class Hashmap{
     private:
     std::vector<std::vector<std::pair<std::string,int>>> buckets;
+    size_t entrycount = 0;
 
     public:
         Hashmap(size_t bucketcount = 10)
@@ -15,7 +17,25 @@ class Hashmap{
             
             size_t bucketindex = hash(key);
             bucketindex %= buckets.size();
+           
+
+            for (auto const& pair : buckets[bucketindex])    
+            {
+                if (pair.first == key)
+                {
+                    pair.second = value;
+                    return;
+                }
+                
+            }
             buckets[bucketindex].push_back({key,value});
+            entrycount++;
+
+            if (static_cast<double>(entrycount) / buckets.size() >= 1.0)
+            {
+                rehash();
+            }
+            
 
 
         }
@@ -78,9 +98,46 @@ class Hashmap{
             }
 
             buckets[bucketindex].push_back({key,v{}});
+            entrycount++;
 
-            return  buckets[bucketindex].back().second;
+            if (static_cast<double>(entrycount) / buckets.size() >= 1.0)
+            {
+                rehash();
+            }
+
+            return  *find(key);
         }
+
+        void rehash(){
+
+            std::vector<std::vector<std::pair<k,v>>> newBuckets(buckets.size() * 2);
+            for (auto const& ob : buckets)     
+            {
+                for (auto const& pair : ob)
+                {
+                    size_t newIndex = hash(pair.first);
+                    newIndex %= newBuckets.size();
+                    newBuckets[newIndex].push_back(pair);
+                   
+                } 
+            }
+             buckets = std::move(newBuckets);
+        }
+
+        size_t size(){
+            return entrycount;
+        }
+
+        size_t bucketCount()const{
+            return buckets.size();
+        }
+
+        double loadFactor() const
+        {
+        return static_cast<double>(entrycount) / buckets.size();
+        }  
+
+
 
 }; 
  
@@ -88,17 +145,7 @@ class Hashmap{
 int main(){
 
     Hashmap<std::string,int> map;
-    map.insert("apple", 5);
-    
-    map["ab"] = 1;
-    map["ba"] = 2;
-    std::cout << map.find("ab");
-    std::cout <<map.find("ba");
-
-
-
-
- return 0;
+ 
 }
 
 size_t hash(const std::string& str){
@@ -109,9 +156,7 @@ size_t hash(const std::string& str){
     {
        hash += c;
     }
-    
 
     return hash;
-
 
 }
